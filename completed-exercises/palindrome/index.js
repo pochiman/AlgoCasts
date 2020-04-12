39f18762eb481b1e418c453a373b96e2fc9af6bc
pochiman <pochiman@yahoo.com>
2020-04-12 18:21:36 -0400

    initial commit

--- completed-exercises/palindrome/index.js
+++ completed-exercises/palindrome/index.js
@@ -8,9 +8,16 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  const reversed = str.split('').reverse().join('');
-
-  return str === reversed;
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
+
+// Solution #1
+// function palindrome(str) {
+//   const reversed = str.split('').reverse().join('');
+//
+//   return str === reversed;
+// }
